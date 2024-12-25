119a9c7b7e6c4b8d509e279c69863b4497323d11
Anamika J Ancheril <anamikajancheril011@gmail.com>
2024-12-25 19:26:05 +0530

    password generator

--- Day6.py
+++ Day6.py
@@ -1,9 +1,22 @@
-for i in range (1,101) :
-    if i%5==0 and i%3==0 :
-        print("fizzBuzz")
-    elif i%5==0 :
-        print("Buzz")
-    elif i%3==0 :
-        print("fizz")
-    else :
-        print(i)
+import random
+letters = ['a','b','c','d','e']
+numbers = ['1','2','3','4','5','6','7','8','9']
+symbols = ['!','@','%']
+
+print("WELCOME TO PYTHON PASSWORD GENERATOR !")
+no_letters=int(input(f"how many letters would you like ?\n"))
+no_numbers=int(input(f"how many numbers would  you like ? \n"))
+no_symbols=int(input(f"how many symbols would you like ?\n"))
+
+password=""
+
+for char in range(0,no_letters ):
+    password += random.choice(letters)
+    
+for char in range(0,no_numbers):
+    password += random.choice(numbers)
+
+for char in range(0,no_symbols):
+    password += random.choice(symbols)
+
+print(password)
